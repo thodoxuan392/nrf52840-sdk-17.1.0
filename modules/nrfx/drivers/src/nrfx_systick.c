459c30c62b0cc256c46eab8d861a09f0feedd2d0
Xuan Tho Do <tho.dx@novaintechs.com>
2024-07-16 23:03:19 +0700

    feat: add systick

--- modules/nrfx/drivers/src/nrfx_systick.c
+++ modules/nrfx/drivers/src/nrfx_systick.c
@@ -122,7 +122,7 @@ void nrfx_systick_init(void)
     nrf_systick_load_set(NRF_SYSTICK_VAL_MASK);
     nrf_systick_csr_set(
         NRF_SYSTICK_CSR_CLKSOURCE_CPU |
-        NRF_SYSTICK_CSR_TICKINT_DISABLE |
+        NRF_SYSTICK_CSR_TICKINT_ENABLE |
         NRF_SYSTICK_CSR_ENABLE);
 }
 
